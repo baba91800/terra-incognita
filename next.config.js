71ed0909d0c15c26b232f20a41d2f55c5173b7ab
baba91800 <baptiste91800@hotmail.fr>
2026-05-16 12:42:37 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
   typescript: {
-    // Cela permet de construire le site même s'il y a des erreurs de types
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Cela ignore les erreurs de style pendant la construction
     ignoreDuringBuilds: true,
   },
-};
-
-export default nextConfig;
+}
+module.exports = nextConfig
